70eda512f114c9cfbf221c9895f90f1c05de5f68
Johannes Christenson <johannes@librem.one>
2020-04-04 13:44:42 +0200

    theme colors and .gitignore

--- config.h
+++ config.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=28"
+	"monospace:pixelsize=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#458588", "#282828" },
-	[SchemeSel] = { "#282828", "#458588" },
+	[SchemeNorm] = { "#bbbbbb", "#282828" },
+	[SchemeSel] = { "#eeeeee", "#550000" },
 	[SchemeOut] = { "#282828", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
